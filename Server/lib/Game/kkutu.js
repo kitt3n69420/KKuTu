d2e6a671429639b75e32eab1cf788cdf31480426
Nyaongi <kitt3n69420@gmail.com>
2026-02-13 05:01:36 +0900

    일단더수정

--- Server/lib/Game/kkutu.js
+++ Server/lib/Game/kkutu.js
@@ -441,9 +441,11 @@ exports.Client = function (socket, profile, sid) {
 		if (!my) return;
 		if (!msg) return;
 
-		JLog.log(`Chan @${channel} Msg #${my.id}: ${msg}`);
 		try { data = JSON.parse(msg); } catch (e) { data = { error: 400 }; }
-		if (Cluster.isWorker) process.send({ type: "tail-report", id: my.id, chan: channel, place: my.place, msg: data.error ? msg : data });
+		if (data.type !== "heartbeat") {
+			JLog.log(`Chan @${channel} Msg #${my.id}: ${msg}`);
+			if (Cluster.isWorker) process.send({ type: "tail-report", id: my.id, chan: channel, place: my.place, msg: data.error ? msg : data });
+		}
 
 		exports.onClientMessage(my, data);
 	});
