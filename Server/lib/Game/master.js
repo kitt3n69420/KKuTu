ba68e80cfd33699cd8e822d4db0c39c6f6cd031c
Nyaongi <kitt3n69420@gmail.com>
2026-03-20 01:07:21 +0900

    ㅁㅇㄴㄹㄹㅇㄴㅇㄹㄴㅁㄻㄴㅇ

--- Server/lib/Game/master.js
+++ Server/lib/Game/master.js
@@ -948,7 +948,16 @@ function processClientRequest($c, msg) {
       break;
     case "updateProfile":
       if (!$c.profile) return;
-      $c.updateProfile(msg || {});
+      var safeProfile = {};
+      // 닉네임은 HTTP /profile 라우트에서만 변경 가능 (소켓 우회 방지)
+      // 소개글만 길이 제한 적용하여 허용
+      if (msg && typeof msg.exordial === 'string') {
+        safeProfile.exordial = msg.exordial.slice(0, 100);
+      }
+      if (msg && typeof msg.nickname === 'string') {
+        safeProfile.nickname = msg.nickname.slice(0, 12);
+      }
+      $c.updateProfile(safeProfile);
       break;
     default:
       break;
--- Server/lib/Web/routes/admin.js
+++ Server/lib/Web/routes/admin.js
@@ -470,12 +470,11 @@ function noticeAdmin(req, ...args) {
   JLog.info(`[ADMIN] ${req.originalUrl} ${req.ip} | ${args.join(" | ")}`);
 }
 function checkAdmin(req, res) {
-  if (req.session.profile) {
-    if (GLOBAL.ADMIN.indexOf(req.session.profile.id) == -1) {
-      req.session.admin = false;
-      return (res.send({ error: 400 }), false);
-    }
-  } else {
+  if (!req.session.profile) {
+    req.session.admin = false;
+    return (res.send({ error: 400 }), false);
+  }
+  if (GLOBAL.ADMIN.indexOf(req.session.profile.id) == -1) {
     req.session.admin = false;
     return (res.send({ error: 400 }), false);
   }
